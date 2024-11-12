34722922c6b789a3d39fee2ca5092467e94c5fce
José Anthony Arenas Conde <142860250+JoseACd744@users.noreply.github.com>
2024-11-12 18:35:39 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const downloadLogRoutes = require('./routes/downloadLogRoutes');
 
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(bodyParser.json());
