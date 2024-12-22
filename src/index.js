d287dca92759d3e276bf104304db6c2ef7fad265
José Anthony Arenas Conde <u20211d744@upc.edu.pe>
2024-12-21 20:41:08 -0500

    Actualiza la configuración de CORS para permitir el acceso desde el entorno de producción

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 8080;
 
 // Configuración de CORS para aceptar cualquier origen y localhost:4200
 const corsOptions = {
-    origin: ['*', 'http://localhost:4200', 'https://victorious-rock-02fabd61e.5.azurestaticapps.net'],
+    origin: ['*', 'http://localhost:4200', 'https://prod-fullstack-hfckgufbhdccgecd.eastus2-01.azurewebsites.net'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
     optionsSuccessStatus: 204
